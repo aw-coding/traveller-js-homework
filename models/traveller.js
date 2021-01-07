343531fb30daf9d39f6eb6b904bfcdda13da4eb4
Andrew Wood <andrewwood90@hotmail.com>
2021-01-07 18:25:16 +0000

    two tests passed

--- models/traveller.js
+++ models/traveller.js
@@ -10,7 +10,9 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return result = this.journeys.map ( (journey) =>{
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
